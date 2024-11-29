a23c1f835fa1a453f813aa0b32c0170945c197db
George Zogopoulos <tailwhipx4@gmail.com>
2024-11-29 12:38:48 +0100

    ArduPlane: Added mode colours

---
+++ ardupilot/plane_mode_colours.lua
@@ -0,0 +1,25 @@
+if v==0 then return "#ffb3e1" end -- MANUAL, pink
+if v==1 then return "#ffffff" end -- CIRCLE        
+if v==2 then return "#cdb3ff" end -- STABILIZE, lilac
+if v==3 then return "#ffffff" end -- TRAINING      
+if v==4 then return "#f9b3ff" end -- ACRO, fushia
+if v==5 then return "#b3b9ff" end -- FLY_BY_WIRE_A, blue
+if v==6 then return "#b3e0ff" end -- FLY_BY_WIRE_B, baby blue
+if v==7 then return "#ffe1b3" end -- CRUISE, orange
+if v==8 then return "#fdffb3" end -- AUTOTUNE, yellow
+if v==10 then return "#b3fffc" end -- AUTO, cyan
+if v==11 then return "#b3ffd9" end -- RTL, jade
+if v==12 then return "#b3e0ff" end -- LOITER, baby blue
+if v==13 then return "#baffb3" end -- TAKEOFF, green
+if v==14 then return "#ffb3b3" end -- AVOID_ADSB, red
+if v==15 then return "#d7ffb3" end -- GUIDED, lime
+if v==16 then return "#ffb3b3" end -- INITIALISING, red  
+if v==17 then return "#cdb3ff" end -- QSTABILIZE, lilac
+if v==18 then return "#b3b9ff" end -- QHOVER, blue
+if v==19 then return "#b3e0ff" end -- QLOITER, baby blue
+if v==20 then return "#ffb3fe" end -- QLAND, purple
+if v==21 then return "#b3ffd9" end -- QRTL, jade
+if v==22 then return "#fdffb3" end -- QAUTOTUNE, yellow
+if v==23 then return "#f9b3ff" end -- QACRO, fushia
+if v==24 then return "#ffffff" end -- THERMAL       
+if v==25 then return "#ffb3fe" end -- LOITER_ALT_QLAND, purple
